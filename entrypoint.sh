e809c5b5b474e9fbeef6d9283832717baeb4d4f3
RONAK BHATT <ronakabhattrz@gmail.com>
2025-06-09 20:18:47 +0530

    feat: add OmniAuth support for Google authentication
    - Add omniauthable to User model\n- Add OmniAuth fields to users table\n- Add from_omniauth method for OAuth handling\n- Add unique index for provider and uid

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails
 rm -f /rails7_boilerplate/tmp/pids/server.pid
-
+ 
 # Then exec the container's main process (what's set as CMD in the Dockerfile)
 exec "$@" 
